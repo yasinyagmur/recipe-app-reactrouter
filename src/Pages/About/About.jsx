30a1286f700a918bf5632f880cb9740896c2afa3
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-18 17:56:27 +0300

    About page compleated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import defaultImage from "../../assets/recipeAbout.png";
+import './about.css'
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="aboutDiv">
+      <img src={defaultImage} alt="" srcset="" />
+      <div className="textAbout">
+        <h3>I'm Yasin.</h3>
+        <p>I'm currently learning Full-Stack Development Languages.</p>
+        <p>
+          I've already known JS, ReactJS, NodeJS, MongoDB, SQL, Python, Agile
+          Methodologies.
+        </p>
+        <p>
+          Send email: <span className="emailText">yasin.yagmur@gmail.com</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
