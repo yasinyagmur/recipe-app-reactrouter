f1943009abf04f7174e784b13213de3d3c3030ce
yasin yağmur <yasin.yagmur@gmail.com>
2022-07-18 18:30:55 +0300

    header component slice navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { NavLink,Link } from 'react-router-dom'
 import './header.css'
-import {FaGithub} from 'react-icons/fa'
+import Navbar from '../Navbar/Navbar'
 const Header = () => {
   return (
     <header>
-      <ul>
-        <li>
-          <a href="http://github.com/yasinyagmur" target="blank">Link For Recipe Project Github <FaGithub size={20}/></a>
-        </li>
-      </ul>
-      <ul>
-        <li><NavLink to='/home' >Recipe</NavLink></li>
-        <li><NavLink to='/about' >About</NavLink></li>
-        <li><Link to='/' >Logout</Link></li>
-      </ul>
+      <Navbar/>
+      
     </header>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+          <a href="http://github.com/yasinyagmur" target="blank">
+            Link For Recipe Project Github <FaGithub size={20} />
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <NavLink to="/home">Recipe</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <Link to="/">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
